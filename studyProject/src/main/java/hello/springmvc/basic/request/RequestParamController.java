991adffe88db74f06d55d9e96e8e1c11c5521522
TaeWook Kim <xodnr2019@naver.com>
2024-03-07 22:28:50 +0900

    Day 10-2 Study : 52강

--- studyProject/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ studyProject/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -4,17 +4,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Make Study. 51강 HTTP 요청 파라미터 - 쿼리 파라미터, HTML Form
+ * Make Study. 52강 요청 파라미터 - @RequestParam
  */
-@Controller
+@RestController
 @Slf4j
 public class RequestParamController {
 
+
+    // Test : Chrome
+    // ?username=kim&age=1
     @RequestMapping("/request-param-v1")
     public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
@@ -23,4 +33,62 @@ public class RequestParamController {
 
         response.getWriter().write("ok");
     }
+
+
+    @RequestMapping("/request-param-v2")
+    public String requestParamV(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age
+    ) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age
+    ) {
+        log.info("username={}, age={}", username, age); //
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-v5")
+    public String requestParamV5(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+
+        // required에 값을 false 할 때, null이 들어갈 수 있는 자료형으로 해야 한다.
+        // int는 null이 들어갈 수가 없다. 그래서, Integer를 넣는다.
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") Integer age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-multimap")
+    public String requestParamMultiMap(@RequestParam MultiValueMap<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
 }
