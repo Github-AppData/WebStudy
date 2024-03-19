2a7a9f451a2957fefe41c87314e796578eeeaca6
TaeWook Kim <xodnr2019@naver.com>
2024-03-19 17:19:17 +0900

    Day 15 Study : 69,70,71,72강

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.SplittableRandom;
@@ -59,12 +60,22 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
-    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
         itemRepository.save(item);
-
-        //model.addAttribute("item", item); @ModelAttribute가 model에 객체를 넣어준다. 자동추가, 생략가능.
-        return "basic/item";
+        return "/basic/items";
+    }
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+    }
+    @PostMapping("/add")
+    public String addItemV4(Item item, @PathVariable RedirectAttributes redirectAttributes) {
+        Item saveditem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", saveditem.getId());
+        redirectAttributes.addAttribute("status",true); // 저장이 됬다는 신호.
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
